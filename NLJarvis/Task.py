fa068136efe4502ab21527ce82b1296e1325c4b8
Mahendra Lohar <mnlohar18@gmail.com>
2023-08-26 10:48:29 +0530

    feat/docs/fix : : Weather feature : Added a new feature which is going to tell you about the weather information of any city you want.

--- NLJarvis/Task.py
+++ NLJarvis/Task.py
@@ -23,6 +23,7 @@ def Day():
     day = datetime.datetime.now().strftime("%A")
     Say(day)
 
+
 def screenshot():
     Say('Name of file')
     name = listen()
@@ -31,9 +32,29 @@ def screenshot():
     Say('Screenshot taken')
 
 
-
-
-
+def weather():
+    # generate your own api key from open weather
+    api_key = "cd9b83f298eba06135ab892cd08d4310"
+    Say("tell me which city")
+    city = listen()
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        current_temperature = data['main']['temp']
+        current_temperature = data['main']["temp"]
+        current_pressure = data['main']["pressure"]
+        current_humidiy = data['main']["humidity"]
+        weather_description = data['weather'][0]['description']
+        r = ("In " + city + " Temperature is " +
+             str(int(current_temperature - 273.15)) + " degree celsius " +
+             ", atmospheric pressure " + str(current_pressure) + " hpa unit" +
+             ", humidity is " + str(current_humidiy) + " percent"
+             " and " + str(weather_description) + " weather")
+        print(r)
+        Say(r)
+    else:
+        Say(" City Not Found ")
 
 
 def NonInputExecution(query):
@@ -47,8 +68,8 @@ def NonInputExecution(query):
         Day()
     elif "screenshot" in query:
         screenshot()
-
-    
+    elif ("weather" in query or "temperature" in query):
+        weather()
 
 
 # Input Function
@@ -60,7 +81,7 @@ def InputExecution(tag, query):
         name = str(query).replace("Who is", "").replace(
             "About", "").replace("What is ", "").replace("wikipedia", "")
         import wikipedia
-        result = wikipedia.summary(name, sentences =2)
+        result = wikipedia.summary(name, sentences=2)
         Say(result)
 
     elif "google" in tag:
